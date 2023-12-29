9132b79dfc4488b3dba658c311b74cbc2e05ab09
usagii <usagii>
2023-12-29 06:17:50 +0000

    created the first command for the bot

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
-print("hello :)")
+import discord
+import os
+import dotenv
+dotenv.load_dotenv()
+token = str(os.getenv("TOKEN"))
+
+bot = discord.Bot()
+
+@bot.event
+async def on_ready():
+    print(f"{bot.user} is ready and online!")
+
+@bot.slash_command(name = "hello", description = "says hello to the user")
+async def hello(ctx):
+    await ctx.respond("Hey!")
+
+bot.run(os.getenv('TOKEN')) #run the bot with the token
