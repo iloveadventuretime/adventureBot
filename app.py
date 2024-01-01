e5b59e1ab70b7dec9ac098156111470127c177d9
usagii <usagii>
2024-01-01 18:11:18 +0000

    made the 'character' command, it gives u the information about the said character and changed the .gitignore

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import songs
+import random
 import discord
 import os
 import dotenv
@@ -5,6 +7,7 @@ dotenv.load_dotenv()
 token = str(os.getenv("TOKEN"))
 
 bot = discord.Bot()
+characters = ['finn', 'marceline', 'jake', 'bmo', 'ice king', 'princess bubblegum', 'lady rainicorn', 'lumpy space princess']
 
 @bot.event
 async def on_ready():
@@ -13,5 +16,20 @@ async def on_ready():
 @bot.slash_command(name = "hello", description = "says hello to the user")
 async def hello(ctx):
     await ctx.respond("Hey!")
+
+@bot.slash_command(name = "songi", description = "testing the random song")
+async def songi(ctx):
+    song = random.randint(0, 9)
+    songURL = "https://open.spotify.com/track/{}"
+    await ctx.respond(songURL.format(songs.songs[song]))
+
+@bot.slash_command(name = "characters", description = "input a character name or 'random' (must be in lowercase)")
+async def character(ctx, char):
+    if char.lower() in characters:
+        charURL = "https://adventuretime.fandom.com/wiki/{}"
+        await ctx.respond(charURL.format(char.lower()))
+    else:
+        await ctx.respond("couldn't find character or you didnt input a character")
+        await ctx.respond(char)
 
 bot.run(os.getenv('TOKEN')) #run the bot with the token
